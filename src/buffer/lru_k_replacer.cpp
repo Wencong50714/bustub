7069924262e0f3be9bc9b12845e4f6d5e74aa258
Ryan <czh50714@gmail.com>
2023-10-07 16:47:53 +0800

    pass the provided test

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -15,16 +15,113 @@
 
 namespace bustub {
 
-LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
+LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {
+  evictable_.reset(new bool[num_frames]);
+  ks_.reset(new size_t[num_frames]);
+  std::fill_n(evictable_.get(), num_frames, false);
+  std::fill_n(ks_.get(), num_frames, 0);
+}
 
-auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
+auto LRUKReplacer::ScanList(std::list<frame_id_t> &l, frame_id_t *frame_id) -> bool {
+  auto it = l.begin();
+  for (; it != l.end(); ++it) {
+    auto fid = *it;
+    if (evictable_[fid]) {
+      *frame_id = fid;
 
-void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType access_type) {}
+      l.erase(it);
+      evictable_[fid] = false;
+      ks_[fid] = 0;
+      return true;
+    }
+  }
+  return false;
+}
 
-void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {}
+auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
+  std::lock_guard<std::mutex> lock(latch_);
 
-void LRUKReplacer::Remove(frame_id_t frame_id) {}
+  if (less_k_list_.empty() && more_k_list_.empty()) {
+    return false;
+  }
 
-auto LRUKReplacer::Size() -> size_t { return 0; }
+  // scan less_k list to find a evict-able one
+  if (ScanList(less_k_list_, frame_id) || ScanList(more_k_list_, frame_id)) {
+    curr_size_--;
+    return true;
+  }
+  return false;
+}
+
+void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType access_type) {
+  std::lock_guard<std::mutex> lock(latch_);
+
+  if (frame_id > static_cast<int>(replacer_size_)) {
+    throw std::exception();
+  }
+
+  // if the frame is new, add to less list
+  if (ks_[frame_id] == 0) {
+
+    less_k_list_.emplace_back(frame_id);
+  }
+
+  ks_[frame_id]++;
+
+  // if frame achieve the k_, remove from less and add to more
+  if (ks_[frame_id] == k_) {
+    auto it = std::find(less_k_list_.begin(), less_k_list_.end(), frame_id);
+    less_k_list_.erase(it);
+    more_k_list_.emplace_back(frame_id);
+  }
+
+  // if already in the more-k list, re-arrange it
+  if (ks_[frame_id] > k_) {
+    auto it = std::find(more_k_list_.begin(), more_k_list_.end(), frame_id);
+    more_k_list_.erase(it);
+    more_k_list_.emplace_back(frame_id);
+  }
+}
+
+void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
+  std::lock_guard<std::mutex> lock(latch_);
+  if (ks_[frame_id] == 0) {
+    // frame not in the context
+    throw std::exception();
+  }
+
+  // alter the curr_size
+  if (set_evictable && !evictable_[frame_id]) {
+    curr_size_++;
+  } else if (!set_evictable && evictable_[frame_id]) {
+    curr_size_--;
+  }
+
+  evictable_[frame_id] = set_evictable;
+}
+
+void LRUKReplacer::Remove(frame_id_t frame_id) {
+  std::lock_guard<std::mutex> lock(latch_);
+
+  if (ks_[frame_id] == 0) {
+    return ;
+  }
+
+  ks_[frame_id] = 0;
+  evictable_[frame_id] = false;
+
+  auto it = std::find(less_k_list_.begin(), less_k_list_.end(), frame_id);
+  if (it != less_k_list_.end()) {
+    less_k_list_.erase(it);
+    return ;
+  }
+
+  it = std::find(more_k_list_.begin(), more_k_list_.end(), frame_id);
+  if (it != more_k_list_.end()) {
+    more_k_list_.erase(it);
+  }
+}
+
+auto LRUKReplacer::Size() -> size_t { return curr_size_; }
 
 }  // namespace bustub
--- src/include/buffer/lru_k_replacer.h
+++ src/include/buffer/lru_k_replacer.h
@@ -25,17 +25,6 @@ namespace bustub {
 
 enum class AccessType { Unknown = 0, Lookup, Scan, Index };
 
-class LRUKNode {
- private:
-  /** History of last seen K timestamps of this page. Least recent timestamp stored in front. */
-  // Remove maybe_unused if you start using them. Feel free to change the member variables as you want.
-
-  [[maybe_unused]] std::list<size_t> history_;
-  [[maybe_unused]] size_t k_;
-  [[maybe_unused]] frame_id_t fid_;
-  [[maybe_unused]] bool is_evictable_{false};
-};
-
 /**
  * LRUKReplacer implements the LRU-k replacement policy.
  *
@@ -148,14 +137,19 @@ class LRUKReplacer {
   auto Size() -> size_t;
 
  private:
+
+  auto ScanList(std::list<frame_id_t> &l, frame_id_t *frame_id) -> bool;
+
   // TODO(student): implement me! You can replace these member variables as you like.
   // Remove maybe_unused if you start using them.
-  [[maybe_unused]] std::unordered_map<frame_id_t, LRUKNode> node_store_;
-  [[maybe_unused]] size_t current_timestamp_{0};
-  [[maybe_unused]] size_t curr_size_{0};
-  [[maybe_unused]] size_t replacer_size_;
-  [[maybe_unused]] size_t k_;
-  [[maybe_unused]] std::mutex latch_;
+  std::list<frame_id_t> less_k_list_;
+  std::list<frame_id_t> more_k_list_;
+  std::shared_ptr<bool[]> evictable_;
+  std::shared_ptr<size_t[]> ks_;
+  size_t curr_size_{0};
+  size_t replacer_size_;
+  size_t k_;
+  std::mutex latch_;
 };
 
 }  // namespace bustub
