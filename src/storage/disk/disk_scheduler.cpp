49ac7b1b3ce28aae559aa440bd573a6b8df3dc02
Ryan <czh50714@gmail.com>
2023-10-08 09:12:12 +0800

    project1_Task2: passed the provided test

--- src/storage/disk/disk_scheduler.cpp
+++ src/storage/disk/disk_scheduler.cpp
@@ -17,11 +17,6 @@
 namespace bustub {
 
 DiskScheduler::DiskScheduler(DiskManager *disk_manager) : disk_manager_(disk_manager) {
-  // TODO(P1): remove this line after you have implemented the disk scheduler API
-  throw NotImplementedException(
-      "DiskScheduler is not implemented yet. If you have finished implementing the disk scheduler, please remove the "
-      "throw exception line in `disk_scheduler.cpp`.");
-
   // Spawn the background thread
   background_thread_.emplace([&] { StartWorkerThread(); });
 }
@@ -34,8 +29,22 @@ DiskScheduler::~DiskScheduler() {
   }
 }
 
-void DiskScheduler::Schedule(DiskRequest r) {}
+void DiskScheduler::Schedule(DiskRequest r) {
+  auto opr = std::make_optional<DiskRequest>(std::move(r));
+  request_queue_.Put(std::move(opr));
+}
 
-void DiskScheduler::StartWorkerThread() {}
+void DiskScheduler::StartWorkerThread() {
+  std::optional<DiskRequest> opr;
+  while ((opr = request_queue_.Get()) != std::nullopt) {
+    auto r = std::move(opr.value());
+    if (r.is_write_) {
+      disk_manager_->WritePage(r.page_id_, r.data_);
+    } else {
+      disk_manager_->ReadPage(r.page_id_, r.data_);
+    }
+    r.callback_.set_value(true);
+  }
+}
 
 }  // namespace bustub
