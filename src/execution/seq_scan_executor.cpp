924cd6328e74d4a16bf4f12fd76e695704fe7f39
Ryan <czh50714@gmail.com>
2023-12-01 10:59:40 +0800

    Project3-Task1, pass seq scan tests

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -14,10 +14,30 @@
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx), plan_(plan) {}
 
-void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
+void SeqScanExecutor::Init() {
+  auto table_info = exec_ctx_->GetCatalog()->GetTable(plan_->table_oid_);
+  table_iter_ = std::make_unique<TableIterator>(table_info->table_->MakeIterator());
+}
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+bool SeqScanExecutor::Next(Tuple *tuple, RID *rid) {
+  // Loop until a valid tuple is found or end of table is reached
+  while (!table_iter_->IsEnd()) {
+    auto [metadata, tuple_data] = table_iter_->GetTuple();
+
+    // If the tuple is not deleted, set the output tuple and RID, then return true
+    if (!metadata.is_deleted_) {
+      *tuple = Tuple(tuple_data);
+      *rid = table_iter_->GetRID();
+      ++(*table_iter_);
+      return true;
+    }
+
+    ++(*table_iter_);
+  }
+  return false;
+}
+
 
 }  // namespace bustub
