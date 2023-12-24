e50e094de8a8f75c6c2530d53acc8fba7510ea1a
Ryan <czh50714@gmail.com>
2023-12-24 21:03:06 +0800

    p4: finished task2

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -11,6 +11,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "execution/executors/seq_scan_executor.h"
+#include "concurrency/transaction_manager.h"
+#include "execution/execution_common.h"
 
 namespace bustub {
 
@@ -20,9 +22,55 @@ SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNod
 void SeqScanExecutor::Init() {
   auto table_info = exec_ctx_->GetCatalog()->GetTable(plan_->table_oid_);
   table_iter_ = std::make_unique<TableIterator>(table_info->table_->MakeIterator());
+
+  if (exec_ctx_->GetTransaction()->GetIsolationLevel() == IsolationLevel::SNAPSHOT_ISOLATION) {
+    ts_ = exec_ctx_->GetTransaction()->GetReadTs();
+    txn_mgr_ = exec_ctx_->GetTransactionManager();
+  }
 }
 
 auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  if (exec_ctx_->GetTransaction()->GetIsolationLevel() == IsolationLevel::SNAPSHOT_ISOLATION) {
+    while (!table_iter_->IsEnd()) {
+      auto [metadata, tuple_data] = table_iter_->GetTuple();
+
+      std::vector<UndoLog> undo_logs{};
+      bool find_end = false;
+
+      auto undo_link = txn_mgr_->GetUndoLink(table_iter_->GetRID()).value();
+      while (undo_link.IsValid()) {
+        auto undo_log = txn_mgr_->GetUndoLog(undo_link);
+
+        if (undo_log.ts_ >= ts_) {
+          undo_logs.push_back(undo_log);
+          find_end = (undo_log.ts_ == ts_);
+        } else {
+          find_end = true;
+        }
+
+        undo_link = undo_log.prev_version_;
+      }
+
+      if (!find_end) {
+        // Collect next tuple
+        ++(*table_iter_);
+        continue;
+      }
+
+      auto op_tuple = ReconstructTuple(&GetOutputSchema(), Tuple(tuple_data), metadata, undo_logs);
+      if (op_tuple == std::nullopt) {
+        return false;
+      }
+
+      *tuple = op_tuple.value();
+      *rid = table_iter_->GetRID();
+      ++(*table_iter_);
+      return true;
+    }
+
+    return false;
+  }
+
   while (!table_iter_->IsEnd()) {
     auto [metadata, tuple_data] = table_iter_->GetTuple();
 
